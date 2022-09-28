bf01ff9d9f1811e8a762bc7c2cd6a5b3e7563d8f
Jordan <jordan.laczkowski23@gmail.com>
2022-09-28 15:35:13 -0600

    added code to users from what the challenged hinted for us to start with

--- models/User.js
+++ models/User.js
@@ -1,15 +1,37 @@
 const { Schema, model } = require("mongoose");
 const thoughtSchema = require("./Thought");
 
-const userSchema = new Schema();
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      pattern: "^\\S+@\\S+\\.\\S+$",
+      format: "email",
+      minLength: 6,
+      maxLength: 128,
+      // TODO: does this need to be different set amounts?
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, reference: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, reference: "User" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-//
-//
-//
-//
-
-const User = model("user", userSchema).get(function () {
+userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-module.exports = User;
+const User = model("User", userSchema);
